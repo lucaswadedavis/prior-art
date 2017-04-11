203da9c8366391d8186d2467fc71f183eb8615cc
lucaswadedavis <lucaswadedavis@gmail.com>
2017-04-10 20:57:39 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prior Art
+
+A GUI for the great _prior_ npm package by 3rdEden.
+
+### License: MIT
---
+++ server.js
@@ -0,0 +1,31 @@
+var port = process.argv[2] || process.env.PORT || 8081;
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const fileName = 'prior-art.json';
+
+app.get('/api', (req, res) => {
+  jsonfile.readFile(fileName,(e, obj) => {
+    if (e) return res.send(400, e);
+    console.log(obj);
+    return res.send(200, obj);
+  });
+});
+
+app.post('/api', (req, res) => {
+  console.log(req.body);
+  jsonfile.writeFile(fileName, req.body, (e) => {
+    return console.log(e)
+  });
+  return res.sendStatus(200, req.body);
+});
+
+app.listen(port, function () {
+    console.log('Server started on port', port);
+});
